355ba973de3374af05c5e45526ed7f95039f4294
FadiBibawy <eng.fadimichel@gmail.com>
2023-01-31 19:48:12 +0100

    routes edited, root added

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  get 'bookmarks/new'
+  # get 'bookmarks/new'
   # get 'lists/index'
   # get 'movies/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :movies
-  resources :lists do
+  # resources :movies
+  root to: 'lists#index'
+  resources :lists, except: [:index] do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: [:destroy]
